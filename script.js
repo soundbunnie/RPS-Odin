081c1e38f17a675072d61e607681817c4b2db306
Aria Elkaim <ariari.elk@gmail.com>
2022-08-05 22:21:23 -0400

    Create function playRound to determine winner/loser

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var options = ["rock", "paper", "scissors"]
 //create empty variable to store computer choice
 var computerChoice
 
+var playerChoice
+
+
 //create function that randomly chooses from options
 function getComputerChoice(){
     //create random number within range of options.length, create variable referring to item index of the result
@@ -12,4 +15,20 @@ function getComputerChoice(){
 }
 
 //ask user to input their choice then convert to lower case
-var playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase()
+function getPlayerChoice(){
+    playerChoice = prompt("Rock, Paper, or Scissors?");
+    return(playerChoice)
+}
+
+//write function to play round
+function playRound(playerChoice, computerChoice){
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+    //create logic gate with winning outcomes
+    if (playerChoice === "paper" && computerChoice === "rock" || playerChoice === "rock" && computerChoice === "scissors" || playerChoice === "scissors" && computerChoice === "paper"){
+        return "You win";
+    }
+    else{
+        return "No win";
+    }
+}
